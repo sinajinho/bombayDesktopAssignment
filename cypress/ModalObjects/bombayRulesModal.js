17cd9163a9a55632e0747b958fafedb2d508b466
Janis <janisgriezans@hotmail.com>
2023-05-26 14:51:03 +0300

    changed ModalObejcts

--- cypress/ModalObjects/bombayRulesModal.js
+++ cypress/ModalObjects/bombayRulesModal.js
@@ -2,6 +2,7 @@ class bombayRulesModal {
   verifyRulesCorrectness() {
     cy.getIFrameBody().as('iframe')
     cy.get('@iframe').find('[data-test-id="navigation-page_scrollable_container"]').should('be.visible').within(() => {
+      cy.get('.scrollbar').should('be.visible').scrollTo('bottom', { ensureScrollable: false })
       cy.get('.subheader').should('be.visible').contains('Game elements:')
       cy.get('.subheader').should('be.visible').contains('Buttons:')
       cy.get('.subheader').should('be.visible').contains('Auto play:')
